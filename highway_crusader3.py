b87f09f7dfad4fb31f1bd760c86de5367017b1b7
georgebalmer <34370631+georgebalmer@users.noreply.github.com>
2020-12-13 19:05:02 +0000

    new version

---
+++ highway_crusader3.py
@@ -0,0 +1,381 @@
+import pygame
+import math
+import random
+# -- Global Constants
+
+
+# -- Colours
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+
+traffic_x_list = [200,400,600]
+
+map = [[0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0]]
+
+
+# Classe definitions
+
+#### Game class
+class Game (pygame.sprite.Sprite):
+    # Instantiation method
+    def __init__(self):
+        super().__init__()
+        self.GameDone = False
+        self.all_sprites_group = pygame.sprite.Group()
+
+        # Create map
+        self.roadedge_list = pygame.sprite.Group()
+        # Create walls on the screen (each tile is 20 x 20 so alter coords)
+        for y in range(38):
+            for x in range (45):
+                if map[y][x] == 1:
+                    my_roadedge = Roadedge(WHITE, 20, 20, x*20, y *20)
+                    self.roadedge_list.add(my_roadedge)
+                    self.all_sprites_group.add(my_roadedge)
+        # Create road marks
+        #road mark creation
+        self.roadmarks = pygame.sprite.Group()
+        roadmark_y_coord = 0
+        roadmark_x_coord = 350
+        counter = 0
+        for y in range(2):
+            for x in range(20):
+                roadmark = Road_mark(WHITE,5,20,roadmark_x_coord,roadmark_y_coord,1)
+                self.all_sprites_group.add(roadmark)
+                self.roadmarks.add(roadmark)
+                roadmark_y_coord += 30
+                counter += 1
+            roadmark_y_coord = 0
+            roadmark_x_coord += 200
+
+        # Create player
+        self.player = Player(RED,100,100,0,0)
+        self.all_sprites_group.add (self.player)
+
+        #create traffic
+        #list for random spawn x co-ord
+        
+        self.traffic_list = pygame.sprite.Group()
+        traffic_counter=0
+        traffic_speedcount=4
+
+        for y in range(3):
+            traffic = Traffic(YELLOW,100,100,random.choice(traffic_x_list),random.randint(40,700)*-1,traffic_speedcount)
+            self.all_sprites_group.add(traffic)
+            self.traffic_list.add(traffic)
+            traffic_counter += 1
+            traffic_speedcount = 4
+
+        #create coins
+        self.coin_list = pygame.sprite.Group()
+        self.coiny = 0
+        for y in range(3):
+            coin = Coin(GREEN, 50, 50, random.choice(traffic_x_list), self.coiny, 4)
+            self.all_sprites_group.add(coin)
+            self.coin_list.add(coin)
+            self.coiny += 50
+
+    def check_colls(self):
+        ## Check traffic collision
+        traffic_hit_list = pygame.sprite.spritecollide(self.player, self.traffic_list, False)
+        for hit in traffic_hit_list:
+            self.GameDone = True
+        lowy = 1000
+        for x in self.traffic_list:
+            if x.rect.y < lowy:
+                lowy = x.rect.y
+
+        ## check coin collision
+
+        ## check bullet collisions
+
+
+
+### end of game class
+
+#coin class
+class Coin(pygame.sprite.Sprite):
+    def __init__(self,color,width,height,x_ref,y_ref,speedy):
+        #constructor
+        super().__init__()
+
+        #creates sprites
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+
+        #sets position of road mark
+        self.rect.x = x_ref
+        self.rect.y = y_ref
+
+        self.coin_speedy = speedy
+    def update(self):
+
+        self.rect.y = self.rect.y + self.coin_speedy
+        if self.rect.y > 600:
+           self.rect.y = random.randint(40,700)*-1
+           self.rect.x = random.choice(traffic_x_list)
+        
+           
+
+    def coin_speedy(self, val):
+        self.coin_speedy = speedy
+        
+
+
+#### Player class
+class Player(pygame.sprite.Sprite):
+
+    # Instantiation method
+    def __init__(self, color, width, height, speedy, speedx):
+
+         super().__init__()
+
+         self.x = width
+         self.y = height
+         self.colour = color
+
+
+         self.image = pygame.Surface([width,height])
+         self.image.fill(color)
+         self.image = pygame.image.load("player2.png")
+
+         self.rect = self.image.get_rect()
+         self.rect.x = 400
+         self.rect.y = 400
+
+         self.player_speedy = speedy
+         self.player_speedx = speedx
+         
+    # update method to change object of this class each time through the loop
+    def update(self):
+        self.rect.y = self.rect.y + self.player_speedy
+        self.rect.x = self.rect.x + self.player_speedx
+
+        #player_old_x = 0
+        #player_old_y = 0
+
+    # Sets the speed of the player horizontally
+    def player_set_speedx(self, val):
+        self.player_speedx = val
+    # End procedure
+
+    # Sets the speed of the player vertically
+    def player_set_speedy(self,val):
+        self.player_speedy = val
+    #End procedure
+#### End of Player class
+
+
+######tile road edge class
+class Roadedge(pygame.sprite.Sprite):
+
+    #instatiation
+    def __init__(self,color, width, height, x_ref, y_ref):
+
+        #sprite constructor
+        super().__init__()
+
+        #creates sprite
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        # Set the position of the road edge attributes
+        self.rect.x = x_ref
+        self.rect.y = y_ref
+
+#### end of road edge class
+
+#### Road Mark class
+
+class Road_mark(pygame.sprite.Sprite):
+        
+    #instantiation
+    def __init__(self,color,width,height,x_ref,y_ref,speedy):
+
+        #constructor
+        super().__init__()
+
+        #creates sprites
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+
+        #sets position of road mark
+        self.rect.x = x_ref
+        self.rect.y = y_ref
+
+        #set speed
+        self.roadmark_speedy = speedy
+        #end procedure
+
+
+    def update(self):
+         
+        self.rect.y = self.rect.y + self.roadmark_speedy
+        if self.rect.y > 600:
+            self.rect.y=0
+            
+            
+    def roadmark_set_speedy(self,val):
+        speedy = val
+        self.roadmark_speedy = speedy
+
+class Traffic(pygame.sprite.Sprite):
+
+    #instantiation
+    def __init__(self,color,width,height,x_ref,y_ref,speedy):
+
+        #constructor
+        super().__init__()
+
+        #creates sprite
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        # Set the position of the traffic attributes
+        self.rect.x = x_ref
+        self.rect.y = y_ref
+
+        self.traffic_speedy = speedy
+
+    def update(self):
+        #traffic_speedcount
+        #traffic_counter
+        self.rect.y = self.rect.y + self.traffic_speedy
+        if self.rect.y > 600:
+            self.rect.y = random.randint(40,700)*-1
+            self.rect.x = random.choice([200,400,600])
+
+    
+    def traffic_set_speedy(self,val):
+        speedy = val
+        self.traffic_speedy = speedy
+
+
+
+# -- Initialise PyGame
+pygame.init()
+
+
+# -- Blank Screen
+size = (900,600)
+
+screen = pygame.display.set_mode(size)
+
+GameDone = False
+clock = pygame.time.Clock()
+g = Game()
+
+while not g.GameDone:
+            
+    # -- User input and controls
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            GameDone = True
+        # end event type
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                g.player.player_set_speedx(-3)
+
+            elif event.key == pygame.K_RIGHT:
+                g.player.player_set_speedx(3)
+
+            elif event.key == pygame.K_UP:
+                g.player.player_set_speedy(-3)    
+
+            elif event.key == pygame.K_DOWN:
+                g.player.player_set_speedy(3)    
+
+            elif event.key == pygame.K_ESCAPE:
+                GameDone = True
+
+            elif event.key == pygame.K_SPACE:
+                pass
+
+        # end event type
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                g.player.player_set_speedx(0)
+
+            elif event.key == pygame.K_RIGHT:
+                g.player.player_set_speedx(0)
+
+            elif event.key == pygame.K_UP:
+                g.player.player_set_speedy(0)
+     
+            elif event.key == pygame.K_DOWN:
+                g.player.player_set_speedy(0)
+                            
+        # end event type
+
+    # end event for loop
+
+
+    # -- Game logic goes after this comment
+    g.all_sprites_group.update()
+    g.check_colls()
+    # Runs the update function for all sprites
+
+    # -- Screen background is BLACK
+    screen.fill (BLACK)
+    # -- Draw here
+    g.all_sprites_group.draw(screen)
+
+    ## Put text on screen
+
+
+    # -- flip display to reveal new position of objects
+    pygame.display.flip()
+    # -- The clock ticks over
+    clock.tick(60)
+            
+          
+#End While
+ 
+
+
